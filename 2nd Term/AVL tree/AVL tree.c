8a5b5f8ecd06948c9155f4c4f0914124c3722570
art9440 <art9440@gmail.com>
2024-02-06 18:40:19 +0700

    start AVL

---
+++ 2nd Term/AVL tree/AVL tree.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num_of_tops, number;
+    scanf("%d", &num_of_tops);
+    for (int i = 0; i < num_of_tops; i++){
+        scanf("%d", &number);
+
+    }
+
+}
