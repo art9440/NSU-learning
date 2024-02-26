7dbd1afb149ea47d26165013d283e18ec732d6d9
art9440 <art9440@gmail.com>
2024-02-26 22:11:03 +0700

    work is done

--- 2nd_Term/AVL_tree/AVL_tree.c
+++ 2nd_Term/AVL_tree/AVL_tree.c
@@ -3,17 +3,6 @@
 #include <string.h>
 
 
-void orders(char* answer, TREE * avl_tree){
-    if (strcmp(answer, "postorder") == 0)
-        post_order(avl_tree);
-    else if (strcmp(answer, "preorder") == 0)
-        pre_order(avl_tree);
-    else if (strcmp(answer, "inorder") == 0)
-        in_order(avl_tree);
-    else
-        puts("incorrect input.");
-}
-
 
 int main(){
     int num_of_tops, number;
@@ -28,11 +17,9 @@ int main(){
             avl_tree = insert(avl_tree,number);
         }
     }
-    printf("%d\n", get_height(avl_tree) + 1);
-    puts("What order do you want?");
-    char answer[10];
-    scanf("%s", &answer);
-    orders(answer, avl_tree);
+    printf("%d\n", treeHeight(avl_tree));
+    puts("In order");
+    in_order(avl_tree);
     del_tree(avl_tree);
 
     return 0;
--- 2nd_Term/AVL_tree/functions.h
+++ 2nd_Term/AVL_tree/functions.h
@@ -11,7 +11,6 @@
 
 TREE{
     int num;
-    int height;
     TREE * left;
     TREE * right;
 };
@@ -23,16 +22,13 @@ int max(int num1, int num2){
     return num2;
 }
 
-
-int get_height(TREE * T){
+int treeHeight(TREE *T){
     if (T == NULL)
-        return -1;
-    return T->height;
-}
+        return 0;
+    int l_height = treeHeight(T->left);
+    int r_height = treeHeight(T->right);
 
-
-void new_height(TREE * T){ //update of height of the element
-    T->height = max(get_height(T->left), get_height(T->right)) + 1;
+    return max(l_height, r_height) + 1;
 }
 
 
@@ -40,8 +36,6 @@ TREE * rotate_right(TREE * T){
     TREE * child = T->left;
     T->left = child -> right;
     child -> right = T;
-    new_height(T);
-    new_height(child);
     return child;
 }
 
@@ -50,8 +44,6 @@ TREE * rotate_left(TREE * T){
     TREE * child = T->right;
     T->right = child -> left;
     child -> left = T;
-    new_height(T);
-    new_height(child);
     return child;
 }
 
@@ -59,12 +51,11 @@ TREE * rotate_left(TREE * T){
 int get_balance(TREE * T){ //getting balance of the element
     if (T == NULL)
         return 0;
-    return get_height(T->right) - get_height(T->left);
+    return treeHeight(T->right) - treeHeight(T->left);
 }
 
 
 TREE * balance(TREE * T){   //checking for balance and balancing of tree
-    new_height(T);
     int bal = get_balance(T);
     if (bal == -2){
         if (get_balance(T->left) == 1)
@@ -84,7 +75,6 @@ TREE * balance(TREE * T){   //checking for balance and balancing of tree
 TREE * create(int number){ //creating element of struct tree
     TREE * cur;
     cur =(TREE*)malloc(sizeof (TREE));
-    cur -> height = 0;
     cur -> num = number;
     cur -> right = NULL;
     cur -> left = NULL;
@@ -105,9 +95,9 @@ TREE * insert(TREE * T, int number){  //inserting element in the tree
         else
             insert(T -> right, number);
     }
-    new_height(T);
     return balance(T);
 }
+
 
 void del_tree(TREE * T){
     if (T != NULL){
@@ -115,24 +105,6 @@ void del_tree(TREE * T){
         del_tree(T->left);
         free(T);
     }
-}
-
-
-void pre_order(TREE *T) {
-    printf("%d\n",T -> num);
-
-    if (T -> left != NULL)
-        pre_order(T -> left);
-    if (T -> right != NULL)
-        pre_order(T -> right);
-}
-
-void post_order(TREE * T){
-    if (T -> left)
-        post_order(T -> left);
-    if (T -> right)
-        post_order(T -> right);
-    printf("%d\n", T -> num);
 }
 
 
