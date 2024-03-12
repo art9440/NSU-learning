6830b8c0a83590ad1f0786ba2b2a983d49ac4295
art9440 <144871779+art9440@users.noreply.github.com>
2024-03-12 23:43:06 +0700

    Work is done

--- 2nd_Term/lab9-0/lab9-0.c
+++ 2nd_Term/lab9-0/lab9-0.c
@@ -3,13 +3,14 @@
 #include "Graph_func.h"
 
 
-int Check_for_errors(int node_count, int edge_count, int start, int finish){
+int Check_for_errors(int node_count, int edge_count,
+                     int start, int finish){              //checking for errors
     if (node_count > 5000 || node_count < 0) {
         puts("bad number of vertices");
         return 0;
     }
 
-    if (edge_count < 0 || edge_count > node_count * (node_count + 1)/2) {
+    if (edge_count < 0 || edge_count > node_count * (node_count + 1)/2){
         puts("bad number of edges");
         return 0;
     }
@@ -24,26 +25,41 @@ int Check_for_errors(int node_count, int edge_count, int start, int finish){
 
 
 int main(){
+    FILE *file = fopen("in.txt", "r");
     int node_count, start, finish, edge_count;
 
-    scanf( "%d", &node_count);
+    if(!fscanf(file,  "%d", &node_count)){
+        fclose(file);
+        return 0;
+    }
 
-    scanf( "%d %d", &start, &finish);
 
-    scanf( "%d", &edge_count);
+    if(fscanf(file,  "%d %d", &start, &finish) != 2){
+        fclose(file);
+        return 0;
+    }
+
+    if(!fscanf(file,  "%d", &edge_count)){
+        fclose(file);
+        return 0;
+    }
 
 
     if (Check_for_errors(node_count, edge_count, start, finish) == 0)
         return 0;
 
-    GRAPH *graph = Create_graph(node_count);     //creating graph with adjacency_list
-
+    GRAPH *graph = Create_graph(node_count,
+                                start, finish); //creating graph
+                                                  //with adjacency_list
     int count = 0;
     for (int i = 0; i < edge_count; i++){
         int st_edge, fn_edge;
         long long int weight_edge;
 
-        scanf( "%d %d %lld", &st_edge,&fn_edge, &weight_edge);
+        if(fscanf(file,  "%d %d %lld", &st_edge,&fn_edge, &weight_edge) != 3){
+            fclose(file);
+            return 0;
+        }
 
         if (weight_edge < 0 || weight_edge > INT_MAX) {
             puts("bad length");
@@ -64,7 +80,7 @@ int main(){
         return 0;
     }
 
-    Djeikstra(graph, start, finish);
-
+    Djeikstra(graph);
+    fclose(file);
     return 0;
 }
