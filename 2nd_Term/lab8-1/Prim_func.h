7b0ec4aa777f875e849b94a1b5675f1ec11539a7
art9440 <art9440@gmail.com>
2024-03-23 01:00:50 +0700

    Prim is started

--- 2nd_Term/lab8-1/Prim_func.h
+++ 2nd_Term/lab8-1/Prim_func.h
@@ -1,8 +1,52 @@
-//
-// Created by user on 19.03.2024.
-//
-
 #ifndef NSU_LEARNING_PRIM_FUNC_H
 #define NSU_LEARNING_PRIM_FUNC_H
 
-#endif //NSU_LEARNING_PRIM_FUNC_H
+#define GRAPH struct graph
+
+GRAPH{
+    int node_count;
+    int edge_count;
+    int * adj_matrix;
+    int * visited;
+};
+
+int min(int a, int b){
+    if (a < b)
+        return a;
+    return b;
+}
+
+
+GRAPH * Create_graph(GRAPH * graph, int node_count, int edge_count){
+    graph = (GRAPH*) malloc(sizeof(GRAPH));
+    graph -> node_count = node_count;
+    graph -> edge_count = edge_count;
+    graph -> adj_matrix = (int *)malloc(node_count * node_count * sizeof(int));
+    graph -> visited = (int*) malloc(node_count * sizeof(int));
+
+    for (int i = 0; i < node_count * node_count; i++)
+        graph -> adj_matrix[i] = 0;
+    for (int i = 0; i < node_count; i++)
+        graph -> visited[i] = 0;
+
+    return graph;
+}
+
+
+void add_edge(int start, int finish, int weight, GRAPH * graph){
+    graph -> adj_matrix[start * graph -> node_count + finish] = weight;
+    graph -> adj_matrix[finish * graph -> node_count + start] = weight;
+}
+
+
+void Prim_alg(GRAPH * graph, int start_node){
+    graph -> visited[start_node] = 1;
+    int node_count = graph -> node_count;
+    int edge_count = graph -> edge_count;
+    int min_weight = INT_MAX;
+    for(int i = start_node * node_count; i < (start_node + 1) * node_count; i++){
+        if (graph -> adj_matrix[start_node * node_count + i] != 0)
+            min_weight = min(min_weight, graph -> adj_matrix[start_node * node_count + i]);
+    }
+}
+#endif
