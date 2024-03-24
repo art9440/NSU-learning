62ce307c9f6fa54c2a4e7b875a6886aebe677606
art9440 <art9440@gmail.com>
2024-03-24 16:49:02 +0700

    Work is done

--- 2nd_Term/lab8-1/Prim_func.h
+++ 2nd_Term/lab8-1/Prim_func.h
@@ -8,13 +8,9 @@ GRAPH{
     int edge_count;
     int * adj_matrix;
     int * visited;
+    int * parents;
+    unsigned int * keys;
 };
-
-int min(int a, int b){
-    if (a < b)
-        return a;
-    return b;
-}
 
 
 GRAPH * Create_graph(GRAPH * graph, int node_count, int edge_count){
@@ -23,11 +19,19 @@ GRAPH * Create_graph(GRAPH * graph, int node_count, int edge_count){
     graph -> edge_count = edge_count;
     graph -> adj_matrix = (int *)malloc(node_count * node_count * sizeof(int));
     graph -> visited = (int*) malloc(node_count * sizeof(int));
+    graph -> parents = (int*) malloc(node_count * sizeof(int));
+    graph -> keys = malloc(node_count * sizeof(unsigned int));
 
     for (int i = 0; i < node_count * node_count; i++)
         graph -> adj_matrix[i] = 0;
-    for (int i = 0; i < node_count; i++)
-        graph -> visited[i] = 0;
+
+    for (int i = 0; i < node_count; i++) {
+        graph->visited[i] = 0;
+        graph -> keys[i] = UINT_MAX;
+    }
+
+    graph -> keys[0] = 0;
+    graph -> parents[0] = -1;
 
     return graph;
 }
@@ -38,15 +42,48 @@ void add_edge(int start, int finish, int weight, GRAPH * graph){
     graph -> adj_matrix[finish * graph -> node_count + start] = weight;
 }
 
-
-void Prim_alg(GRAPH * graph, int start_node){
-    graph -> visited[start_node] = 1;
-    int node_count = graph -> node_count;
-    int edge_count = graph -> edge_count;
-    int min_weight = INT_MAX;
-    for(int i = start_node * node_count; i < (start_node + 1) * node_count; i++){
-        if (graph -> adj_matrix[start_node * node_count + i] != 0)
-            min_weight = min(min_weight, graph -> adj_matrix[start_node * node_count + i]);
+int min_key(GRAPH * graph){
+    unsigned int min_key = UINT_MAX;
+    int min_index;
+    for (int i = 0; i < graph -> node_count; i++){
+        if (graph -> visited[i] == 0 && graph -> keys[i] < min_key){
+            min_key = graph -> keys[i];
+            min_index = i;
+        }
     }
+    return min_index;
+}
+
+
+void print_Prim(GRAPH * graph){
+    for (int i = 1; i < graph -> node_count; i++){
+        printf("%d %d\n", graph -> parents[i] + 1, i + 1);
+    }
+}
+
+
+void Prim_alg(GRAPH * graph){
+    int node_count = graph -> node_count;
+
+    for (int i = 0; i < node_count - 1; i++){
+        int visit = min_key(graph);
+
+        graph -> visited[visit] = 1;
+        for (int j = 0; j < node_count; j++){
+            if (graph -> adj_matrix[visit * node_count + j]
+            && graph -> visited[j] == 0 &&
+            graph -> adj_matrix[visit * node_count + j] < graph -> keys[j]) {
+                graph->parents[j] = visit;
+                graph->keys[j] = graph->adj_matrix[visit * node_count + j];
+            }
+        }
+    }
+    for (int i = 0; i < node_count; i++){
+        if (graph -> keys[i] == UINT_MAX) {
+            puts("no spanning tree");
+            return;
+        }
+    }
+    print_Prim(graph);
 }
 #endif
