6904109b9e6adb4538b7c64a56b3eaa78b1b81ba
art9440 <art9440@gmail.com>
2024-04-25 10:33:36 +0700

    Huffman is started

--- 2nd_Term/Huffman/Huffman.c
+++ 2nd_Term/Huffman/Huffman.c
@@ -7,7 +7,6 @@
 
 #define QUEUE struct queue
 #define NODE struct node
-#define FIRST_MEET 1
 
 NODE{
     wchar_t symbol;
@@ -22,6 +21,43 @@ QUEUE{
     NODE ** heap_for_huffman;
 };
 
+
+void print_queue(QUEUE * queue){
+    for (int i = 0; i < queue->size; i++){
+        wchar_t s = queue->heap_for_huffman[i]->symbol;
+        long int f = queue->heap_for_huffman[i]->freq;
+        printf("%lc --- %ld", s, f);
+    }
+}
+
+
+void update_size(QUEUE * queue){
+    queue->size++;
+    queue->heap_for_huffman = realloc(queue->heap_for_huffman,
+                    queue->size * sizeof(NODE*));
+}
+
+
+void add_node(wchar_t symbol, QUEUE * queue){
+    NODE * new_node = (NODE*)malloc(sizeof(NODE));
+    new_node->symbol = symbol;
+    new_node->freq = 1;
+    new_node->right = NULL;
+    new_node->left = NULL;
+    int index = queue->size - 1;
+    queue->heap_for_huffman[index] = new_node;
+
+    while (index > 0 && queue->heap_for_huffman[index]
+    >= queue->heap_for_huffman[index-1]){
+        NODE * temp =  queue->heap_for_huffman[index];
+        queue->heap_for_huffman[index] =
+                queue->heap_for_huffman[index - 1];
+        queue->heap_for_huffman[index - 1] = temp;
+        index--;
+    }
+}
+
+
 void Creating_queue(QUEUE * queue){
     queue = (QUEUE*) malloc(sizeof(QUEUE));
     queue->size = 0;
@@ -34,11 +70,30 @@ void reading_file(FILE * input) {
     QUEUE *priority_queue = NULL;
     Creating_queue(priority_queue);
     while ((symbol = fgetwc(input)) != WEOF) {
+        printf("%lc", symbol);
         int check_in_queue = 0;
         for (int i = 0; i < priority_queue->size; i++){
-            if ()
+            if (priority_queue->heap_for_huffman[i]->symbol == symbol) {
+                priority_queue->heap_for_huffman[i]->freq++;
+                check_in_queue = 1;
+
+                while (i > 0 && priority_queue->heap_for_huffman[i]
+                ->freq < priority_queue->heap_for_huffman[i - 1]->freq){
+                    NODE *temp = priority_queue->heap_for_huffman[i];
+                    priority_queue->heap_for_huffman[i] = priority_queue
+                            ->heap_for_huffman[i - 1];
+                    priority_queue->heap_for_huffman[i - 1] = temp;
+                }
+                check_in_queue = 1;
+                break;
+            }
+        }
+        if (!check_in_queue) {
+            update_size(priority_queue);
+            add_node(symbol, priority_queue);
         }
     }
+    print_queue(priority_queue);
 }
 
 
