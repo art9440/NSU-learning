cb865db73911e6aaa6ec1e47628c23f5266fc6dd
art9440 <144871779+art9440@users.noreply.github.com>
2024-05-04 21:10:31 +0700

    all

--- 2nd_Term/cmake-build-debug/cmake_install.cmake
+++ 2nd_Term/cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/user/AppData/Local/JetBrains/CLion 2023.2.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/CLion 2023.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
